3e6832845e39f57670bd64fbd7b6e3da5f5f5a17
Rupert Bedford <182958+rupert@users.noreply.github.com>
2021-03-03 10:23:33 +0000

    Add E2E test

---
+++ tests/e2e/test_list_areas.py
@@ -0,0 +1,8 @@
+import requests
+
+BASE_URL = "http://localhost:5000"
+
+
+def test_list_areas_returns_200():
+    response = requests.get(f"{BASE_URL}/areas")
+    assert response.status_code == 200
